f95d5964d9ddf7d5c4a5d8c9d1d3d332b71c5cc8
psvekb <47990165+psvekb@users.noreply.github.com>
2019-12-26 20:27:38 +0500

    1

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
-def displayBoard(board):
+def display_board(board):
+    print('\n' * 10)
     print(board[7], ' | ', board[8], ' | ', board[9], sep='')
     print('---------')
     print(board[4], ' | ', board[5], ' | ', board[6], sep='')
@@ -6,52 +7,81 @@ def displayBoard(board):
     print(board[1], ' | ', board[2], ' | ', board[3], sep='')
 
 
-def playerInput(chainNumb):
-    pPos = 0
-    while pPos > 9 or pPos < 1 or gameBoard[pPos] != ' ':
-        print('Player ', chainNumb, ' - Select position from 1 to 9: ', end='')
-        sPos = input()
-        if sPos.isdigit():
-            pPos = int(sPos)
+def player_input(chain_numb, chain_sign):
+    p_pos = 0
+    while p_pos > 9 or p_pos < 1 or game_board[p_pos] != ' ':
+        print('Player ', chain_numb, ' - Select ', chain_sign, 'position from 1 to 9: ', end='')
+        s_pos = input()
+        if s_pos.isdigit():
+            p_pos = int(s_pos)
+        display_board(game_board)
         print('Error input, not digit or position is not possible')
-        displayBoard(gameBoard)
-    return pPos
+    return p_pos
 
 
-def checkWin(pSign):
-    if gameBoard[1] == gameBoard[2] == gameBoard[3] == pSign:
-        return pSign
-    elif gameBoard[4] == gameBoard[5] == gameBoard[6] == pSign:
-        return pSign
-    elif gameBoard[7] == gameBoard[8] == gameBoard[9] == pSign:
-        return pSign
+def check_win(p_sign):
+    if game_board[1] == game_board[2] == game_board[3] == p_sign:
+        return p_sign
+    elif game_board[4] == game_board[5] == game_board[6] == p_sign:
+        return p_sign
+    elif game_board[7] == game_board[8] == game_board[9] == p_sign:
+        return p_sign
+    elif game_board[1] == game_board[4] == game_board[7] == p_sign:
+        return p_sign
+    elif game_board[2] == game_board[5] == game_board[8] == p_sign:
+        return p_sign
+    elif game_board[3] == game_board[6] == game_board[9] == p_sign:
+        return p_sign
+    elif game_board[1] == game_board[5] == game_board[9] == p_sign:
+        return p_sign
+    elif game_board[7] == game_board[5] == game_board[3] == p_sign:
+        return p_sign
     else:
         return '#'
 
 
-testBoard = ['#', 'O', 'X', 'O', 'X', 'X', 'O', 'X', 'X', 'O']
-gameBoard = ['#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+# test_board = ['#', 'O', 'X', 'O', 'X', 'X', 'O', 'X', 'X', 'O']
+# game_board = ['#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
-print('Tic Tac Toe')
-p1Sign = input('Player 1 - Select X or O: ')
-while p1Sign != 'X' and p1Sign != 'O':
-    print('Error input')
-    p1Sign = input('Select X or O: ')
-if p1Sign == 'X':
-    p2Sign = 'O'
-else:
-    p2Sign = 'X'
-
-for i in range(1, 9 + 1):
-    print('\n' * 10)
-    displayBoard(gameBoard)
-    chainPlayer = (i + 1) % 2 + 1
-    p1Pos = playerInput(chainPlayer)
-    if chainPlayer == 1:
-        gameBoard[p1Pos] = p1Sign
+while 1:
+    game_board = ['#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    display_board(game_board)
+    print('Tic Tac Toe')
+    p1_sign = input('Player 1 - Select X or O: ')
+    while p1_sign != 'X' and p1_sign != 'O':
+        print('Error input')
+        p1_sign = input('Select X or O: ')
+    if p1_sign == 'X':
+        p2_sign = 'O'
     else:
-        gameBoard[p1Pos] = p2Sign
-    if checkWin(p1Sign) != '#':
+        p2_sign = 'X'
+
+    chain_player = 1
+    p_sign = p1_sign
+    for i in range(1, 9 + 1):
+        display_board(game_board)
+        chain_player = (i + 1) % 2 + 1
+        if chain_player == 1:
+            p_pos = player_input(chain_player, p1_sign)
+            game_board[p_pos] = p1_sign
+        else:
+            p_pos = player_input(chain_player, p2_sign)
+            game_board[p_pos] = p2_sign
+        if check_win(p1_sign) != '#':
+            p_sign = p1_sign
+            break
+        if check_win(p2_sign) != '#':
+            p_sign = p2_sign
+            break
+
+    display_board(game_board)
+    print('Player', chain_player, '-', p_sign, 'wins')
+
+    p1_sign = input('New game - select y or n to qiut: ')
+    while p1_sign != 'y' and p1_sign != 'n':
+        print('Error input')
+        p1_sign = input('New game - select y or n to qiut: ')
+    if p1_sign == 'n':
         break
-displayBoard(gameBoard)
-print('Player', chainPlayer, '-', p1Sign, 'wins')
+
+print('End game')
